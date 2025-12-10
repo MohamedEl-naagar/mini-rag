55a887cc54956a723fdbd9a06f1d23a3c561c491
Mohamed-Elnaggar95 <mohamednegoo5@gmail.com>
2025-12-10 14:16:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="postgres",  # Replace with your database name
+            user="postgres",          # Replace with your PostgreSQL username
+            password="root"       # Replace with your PostgreSQL password
+        )
+        print("Connected to PostgreSQL successfully!")
+
+        # You can now perform database operations using the 'conn' object
+
+except psycopg2.Error as e:
+        print(f"Error connecting to PostgreSQL: {e}")
+
+finally:
+        if 'conn' in locals() and conn:
+            conn.close()
+            print("Connection closed.")
