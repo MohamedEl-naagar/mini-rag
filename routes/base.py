b4203f002aa21fdea8d14180e448228d5df463f1
Mohamed-Elnaggar95 <mohamednegoo5@gmail.com>
2025-12-13 21:59:41 +0200

    infrastructure for folders mvc model

--- routes/base.py
+++ routes/base.py
@@ -1,16 +1,20 @@
 # routes
 
-from fastapi import FastAPI,APIRouter
-import os
+# routes.py
+
+from fastapi import APIRouter, Depends
+from helper.config import Settings, get_settings
+
 base_router = APIRouter(
     prefix="/api/v1",
     tags=["api_v1"]
 )
 
 @base_router.get("/")
-async def welcome():
-    app_name = os.getenv("APP_NAME")
-    app_version = os.getenv("APP_VERSION")
-    return{
-        "message":f"Hello, and welcome to our {app_name} system version {app_version}! 🤖"
+async def welcome(app_settings: Settings = Depends(get_settings)):
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
+
+    return {
+        "message": f"Hello, and welcome to our {app_name} system version {app_version}! 🤖"
     }
